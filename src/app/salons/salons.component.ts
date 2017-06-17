d38732eb7aa886ca5ffa1f6df0c1dc6a86aefd5d
Fakhraddin Panakhov <seferbine@yandex.ru>
2017-06-17 09:37:09 +0200

    Salon-search component

--- src/app/salons/salons.component.ts
+++ src/app/salons/salons.component.ts
@@ -2,43 +2,64 @@ import { Component, OnInit } from '@angular/core';
 import { SalonsService } from './salons.service';
 
 @Component({
-  selector: 'app-salons',
-  templateUrl: './salons.component.html',
-  styleUrls: ['./salons.component.css'],
-  providers: [SalonsService]
+    selector: 'app-salons',
+    templateUrl: './salons.component.html',
+    styleUrls: ['./salons.component.css'],
+    providers: [SalonsService]
 })
 
 export class SalonsComponent implements OnInit {
 
-  location:string = 'Munich';
-  
-  private salons;
+    location: String = 'Munich';
+    distance: Number = 1;
+    
+    private salons;
 
-  constructor(private salonsService: SalonsService) { }
+    constructor(private salonsService: SalonsService) { }
   
-  getSalons(){
-    //return this.salonsService.getSalonsLocal().then(salons => {
-    //  this.salons = salons;
-    //});
+    locationMatches(salon){
+        let l = this.location;
+        if(typeof l =='undefined' || !l){
+            return true;
+        }
+        // TODO: check distance with googlemaps
+        return salon.address == l;
+    }
+    
+    dateMatches(salon){
+        return true; // TODO
+    }
+    
+    fromtoMatches(salon){
+        return true; //TODO
+    }
+  
+    getSalons(){
+        //return this.salonsService.getSalonsLocal().then(salons => {
+        //  this.salons = salons;
+        //});
     
     this.salonsService.getSalons()
-      .subscribe(
-        salons => {
-            var arr: any[] = [];
-            salons.filter(s => s.address == this.location).map(s => arr.push(s));
-            this.salons = arr;
-        },
-        error => console.error('Error: ' + error),
-        () => console.log('Completed!')
-      );;
-  }
-  
-  search(){
-    this.getSalons();
-  }
-
-  ngOnInit() {
-    this.getSalons();
-  }
-
+        .subscribe(
+            salons => {
+                var arr: any[] = [];
+                salons
+                    .filter(s => this.locationMatches(s))
+                    .filter(s => this.dateMatches(s))
+                    .filter(s => this.fromtoMatches(s))
+                    .map(s => arr.push(s));
+                this.salons = arr;
+            },
+            error => console.error('Error: ' + error),
+            () => console.log('Completed!')
+        );
+    }
+    
+    search(){
+        this.getSalons();
+    }
+    
+    ngOnInit() {
+        this.getSalons();
+    }
 }
