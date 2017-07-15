ec005aea7882e7b299b8f6850ee835b4924b8376
Fakhraddin Panakhov <seferbine@yandex.ru>
2017-07-15 13:22:23 +0200

    Navigation from Salon Search to SalonBooking

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {MenuItem} from '../models/menuitem'
+import {MenuItem} from '../models/menuitem';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-result',
@@ -22,10 +23,29 @@ export class SearchResultComponent implements OnInit {
   @Input()
   totalPriceForSelectedServices: number;
   
+  @Input()
+  date: any;
+  @Input()
+  salon_id: string;
+  
   selectedTimeSlot:string;
 
-  constructor() {
+  constructor(private router: Router) {
     console.log(this.menu);
+  }
+  
+  goToBooking(){
+    let yearstr = this.date.getFullYear().toString();
+    let monthstr = this.date.getMonth().toString();
+    if (monthstr.length == 1) monthstr = '0' + monthstr;
+    let datestr = this.date.getDate();
+    if (datestr.length == 1) datestr = '0' + datestr;
+    
+    this.router.navigateByUrl(
+			'/salons/' + this.salon_id
+			+'/book?date='
+			+ yearstr + '-' + monthstr + '-' + datestr);
+	return false;
   }
   
   public get topThreeTimeslots() {
